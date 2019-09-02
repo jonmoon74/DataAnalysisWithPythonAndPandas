4c97e7da070a5580219cda767fc1b55ff24794f9
jonmoon74 <jon.moon@outlook.com>
2019-09-02 20:45:39 +0100

    Handling archives

---
+++ extracting_archives.py
@@ -0,0 +1,6 @@
+import patoolib
+import os
+
+os.chdir("/Users/jonmoon/Documents/Coding/Python_stuff/Data Analysis with Python and Pandas/Archive files")
+
+patoolib.extract_archive("File3.tar", outdir="unpacked")
